811da838af2e0829bc188edcc7ed3db1330ba3ea
vinhnx <vinhnx@amirdetail.com>
2021-08-29 01:37:22 +0700

    Day12: VueCli + Vuetify -> Done

---
+++ VUE/vue-quiz/src/App.vue
@@ -0,0 +1,112 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+      <v-container class="py-0 fill-height">
+        <v-avatar
+          class="mr-10"
+          size="32"
+        >
+          <div class="v-image__image v-image__image--cover" style="background-image: url(&quot;https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-light-text.svg&quot;); background-position: left;"></div>
+        </v-avatar>
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list color="transparent">
+                <v-list-item
+                  v-for="n in 5"
+                  :key="n"
+                  link
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      List Item {{ n }}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item
+                  link
+                  color="grey lighten-4"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      Refresh
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!-- Main -->
+              <v-main>
+                <HelloWorld/>
+                <Question/>
+              </v-main>
+              <!-- End Main -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import Question from './components/Question';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    Question
+  },
+
+  data: () => ({
+    links: [
+      'Dashboard',
+      'Messages',
+      'Profile',
+      'Updates',
+    ],
+  }),
+};
+</script>
---
+++ VUE/vue-quiz/src/components/Question.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <h1>Manager library Questions</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+
+    },
+    data(){
+        return{
+
+        }
+    },
+    methods(){
+
+    }
+};
+</script>
+
+<style>
+
+</style>
